c57cbfc4189f29afbccee5397c0f05b4d920bab3
lucabecci <beccibrian@gmail.com>
2025-01-01 23:52:56 -0300

    Add unit tests for depth-first search (DFS) and breadth-first search (BFS) in DirectGraph

--- src/test/kotlin/dsa/DirectGraphTest.kt
+++ src/test/kotlin/dsa/DirectGraphTest.kt
@@ -71,4 +71,102 @@ class DirectGraphTest {
         assertEquals(vertexC, dfsResult[2])
         assertEquals(vertexD, dfsResult[3])
     }
+
+    /**
+     * A first.
+     * Then B, C, F (left side).
+     * Then D, E (right side).
+     */
+    @Test
+    fun `Get the correct path with DFS for letters`() {
+        val vertexA = "A"
+        val vertexB = "B"
+        val vertexC = "C"
+        val vertexD = "D"
+        val vertexE = "E"
+        val vertexF = "F"
+
+        val graph = DirectGraph<String>()
+        graph.addListOfVertex(listOf(
+            vertexA, vertexB, vertexC, vertexD, vertexE, vertexF
+        ))
+
+        graph.addEdge(vertexA, vertexB)
+        graph.addEdge(vertexB, vertexC)
+        graph.addEdge(vertexB, vertexD)
+        graph.addEdge(vertexD, vertexE)
+        graph.addEdge(vertexC, vertexF)
+
+        val dfsResult = graph.depthFirstSearch(vertexA).toList()
+
+        assertEquals(6, dfsResult.size)
+        assertEquals(listOf(vertexA, vertexB, vertexC, vertexF, vertexD, vertexE), dfsResult)
+    }
+
+
+    @Test
+    fun `Get the correct path with BFS`() {
+        val vertexA = "A"
+        val vertexB = "B"
+        val vertexC = "C"
+        val vertexD = "D"
+
+        val directGraph = DirectGraph<String>()
+        directGraph.addListOfVertex(listOf(
+            vertexA,
+            vertexB,
+            vertexC,
+            vertexD,
+        ))
+
+        directGraph.addEdge(vertexA, vertexB)
+        directGraph.addEdge(vertexB, vertexC)
+        directGraph.addEdge(vertexC, vertexD)
+
+        val bfsResult = directGraph.breadthFirstSearch(vertexA).toList()
+
+        assertEquals(4, bfsResult.size)
+        assertEquals(vertexA, bfsResult[0])
+        assertEquals(vertexB, bfsResult[1])
+        assertEquals(vertexC, bfsResult[2])
+        assertEquals(vertexD, bfsResult[3])
+    }
+
+
+    /**
+     * A
+     * B, C, D (level 1, direct neighbors of A).
+     * E, F, G, H (level 2, neighbors of B, C, y D).
+     */
+    @Test
+    fun `Validate BFS with multiple edges per vertex`() {
+        val vertexA = "A"
+        val vertexB = "B"
+        val vertexC = "C"
+        val vertexD = "D"
+        val vertexE = "E"
+        val vertexF = "F"
+        val vertexG = "G"
+        val vertexH = "H"
+
+        val graph = DirectGraph<String>()
+        graph.addListOfVertex(listOf(
+            vertexA, vertexB, vertexC, vertexD, vertexE, vertexF, vertexG, vertexH
+        ))
+
+        graph.addEdge(vertexA, vertexB)
+        graph.addEdge(vertexA, vertexC)
+        graph.addEdge(vertexA, vertexD)
+        graph.addEdge(vertexB, vertexE)
+        graph.addEdge(vertexB, vertexF)
+        graph.addEdge(vertexC, vertexG)
+        graph.addEdge(vertexD, vertexH)
+
+        val bfsResult = graph.breadthFirstSearch(vertexA).toList()
+
+        assertEquals(8, bfsResult.size)
+        assertEquals(listOf(vertexA, vertexB, vertexC, vertexD, vertexE, vertexF, vertexG, vertexH), bfsResult)
+    }
+
+
 }
